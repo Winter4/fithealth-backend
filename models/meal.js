9d1c1afcaf9830c6cdd82b47e18afdeffb18681f
Winter4 <mywinter4@gmail.com>
2022-06-10 12:50:36 +0300

    typescript should wait

--- models/meal.js
+++ models/meal.js
@@ -1,40 +1,40 @@
-const mongoose = require('mongoose');
+const mongoose = require('mongoose')
 
 const mealSchema = new mongoose.Schema({
-    _id: {
-        type: mongoose.Schema.Types.ObjectId,
-    },
-    name: {
-        type: String,
-        required: true,
-    },
-    calories: {
-        type: Number,
-        required: true,
-    },
-    proteins: {
-        type: Number,
-        required: true,
-    },
-    fats: {
-        type: Number,
-        required: true,
-    },
-    carbons: {
-        type: Number,
-        required: true,
-    },
+  _id: {
+    type: mongoose.Schema.Types.ObjectId
+  },
+  name: {
+    type: String,
+    required: true
+  },
+  calories: {
+    type: Number,
+    required: true
+  },
+  proteins: {
+    type: Number,
+    required: true
+  },
+  fats: {
+    type: Number,
+    required: true
+  },
+  carbons: {
+    type: Number,
+    required: true
+  },
 
-    group: {
-        type: String,
-        immutable: true,
-    },
-    title: {
-        type: String,
-        immutable: true,
-    },
-    plan: [Boolean]
-    
-}, { collection: 'meals', versionKey: false });
+  group: {
+    type: String,
+    immutable: true
+  },
+  title: {
+    type: String,
+    immutable: true
+  },
+  plan: [Boolean]
 
-module.exports = mongoose.model('meal', mealSchema);
+}, { collection: 'meals', versionKey: false })
+
+module.exports = mongoose.model('meal', mealSchema)
--- models/user.js
+++ models/user.js
@@ -1,130 +1,130 @@
-const mongoose = require('mongoose');
+const mongoose = require('mongoose')
 
 // _____________________________________
 
 // ATTENTION: if changing this, also change
 //            same limits in /scenes/setters/scene_name
 // i couldn't make it run importing these const from the /scenes/setters/scene_name
-const weightLimits = { min: 40, max: 200 };
-const heightLimits = { min: 120, max: 230 };
-const ageLimits = { min: 13, max: 80 };
+const weightLimits = { min: 40, max: 200 }
+const heightLimits = { min: 120, max: 230 }
+const ageLimits = { min: 13, max: 80 }
 
-const measureLimits = { min: 20, max: 200 };
+const measureLimits = { min: 20, max: 200 }
 
 // _____________________________________
 
 const userSchema = new mongoose.Schema({
-    _id: {
-        type: Number,
-        required: true,
-    },
-    tgUsername: {
-        type: String,
-    },
-    name: {
-        type: String,
-    },
-    sex: {
-        type: String,
-        enum: [
-            'Мужской',
-            'Женский',
-        ]
-    },
-    startWeight: {
-        type: Number,
-        min: weightLimits.min,
-        max: weightLimits.max,
-    },
-    currentWeight: {
-        type: Number,
-        min: weightLimits.min,
-        max: weightLimits.max,
-    },
-    targetWeight: {
-        type: Number,
-        min: weightLimits.min,
-        max: weightLimits.max,
-    },
-    height: {
-        type: Number,
-        min: heightLimits.min,
-        max: heightLimits.max,
-    },
-    age: {
-        type: Number,
-        min: ageLimits.min,
-        max: ageLimits.max,
-    },
-    activity: {
-        type: Number,
-    },
-    caloriesToLose: {
-        type: Number,
-    },
-    chestMeasure: {
-        type: Number,
-        min: measureLimits.min,
-        max: measureLimits.max,
-    },
-    waistMeasure: {
-        type: Number,
-        min: measureLimits.min,
-        max: measureLimits.max,
-    },
-    hipMeasure: {
-        type: Number,
-        min: measureLimits.min,
-        max: measureLimits.max,
-    },
+  _id: {
+    type: Number,
+    required: true
+  },
+  tgUsername: {
+    type: String
+  },
+  name: {
+    type: String
+  },
+  sex: {
+    type: String,
+    enum: [
+      'Мужской',
+      'Женский'
+    ]
+  },
+  startWeight: {
+    type: Number,
+    min: weightLimits.min,
+    max: weightLimits.max
+  },
+  currentWeight: {
+    type: Number,
+    min: weightLimits.min,
+    max: weightLimits.max
+  },
+  targetWeight: {
+    type: Number,
+    min: weightLimits.min,
+    max: weightLimits.max
+  },
+  height: {
+    type: Number,
+    min: heightLimits.min,
+    max: heightLimits.max
+  },
+  age: {
+    type: Number,
+    min: ageLimits.min,
+    max: ageLimits.max
+  },
+  activity: {
+    type: Number
+  },
+  caloriesToLose: {
+    type: Number
+  },
+  chestMeasure: {
+    type: Number,
+    min: measureLimits.min,
+    max: measureLimits.max
+  },
+  waistMeasure: {
+    type: Number,
+    min: measureLimits.min,
+    max: measureLimits.max
+  },
+  hipMeasure: {
+    type: Number,
+    min: measureLimits.min,
+    max: measureLimits.max
+  },
 
-    created: {
-        type: Date,
-        required: true,
-        immutable: true,
-        default: () => Date.now(),
-    },
-    updated: {
-        type: Date,
-        required: true,
-        default: () => Date.now(),
-    },
+  created: {
+    type: Date,
+    required: true,
+    immutable: true,
+    default: () => Date.now()
+  },
+  updated: {
+    type: Date,
+    required: true,
+    default: () => Date.now()
+  },
 
-    state: String,
-    registered: Boolean,
-    paid: Boolean,
-    checked: {
-        date: Number,
-        bool: Boolean,
-    },
-    mealsPerDay: Number,
-    weeksCount: Number,
+  state: String,
+  registered: Boolean,
+  paid: Boolean,
+  checked: {
+    date: Number,
+    bool: Boolean
+  },
+  mealsPerDay: Number,
+  weeksCount: Number
 
-}, {versionKey: false, collection: 'users'} );
+}, { versionKey: false, collection: 'users' })
 
-userSchema.pre('save', function() {
-    this.updated = Date.now();
-});
+userSchema.pre('save', function () {
+  this.updated = Date.now()
+})
 
 // calcs the calories number the user should eat
 // per day to loose his weight
-userSchema.methods.calcCalories = function() {
-    try {
-        const sexParam = this.sex == 'Мужской' ? 5 : -161;
-        let basicCaloricIntake =
+userSchema.methods.calcCalories = function () {
+  try {
+    const sexParam = this.sex == 'Мужской' ? 5 : -161
+    let basicCaloricIntake =
             10 * this.currentWeight +
             6.25 * this.height -
             5 * this.age +
             sexParam
-        ;
-        basicCaloricIntake *= this.activity;
 
-        this.caloriesToLose = basicCaloricIntake * 0.9;
-        this.caloriesToLose *= 1 - (this.weeksCount * 0.1);
-        this.caloriesToLose = (this.caloriesToLose).toFixed();
-    } catch (e) {
-        throw new Error(`Error in <userSchema.calcCalories> of <models/user> file --> ${e.message}`);
-    }
-};
+    basicCaloricIntake *= this.activity
 
-module.exports = mongoose.model('user', userSchema);
+    this.caloriesToLose = basicCaloricIntake * 0.9
+    this.caloriesToLose *= 1 - (this.weeksCount * 0.1)
+    this.caloriesToLose = (this.caloriesToLose).toFixed()
+  } catch (e) {
+    throw new Error(`Error in <userSchema.calcCalories> of <models/user> file --> ${e.message}`)
+  }
+}
+
+module.exports = mongoose.model('user', userSchema)
