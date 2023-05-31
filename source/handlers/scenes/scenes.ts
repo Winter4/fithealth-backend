0ce23095970153454eee7f83c4540e03f1e398a2
Winter4 <mywinter4@gmail.com>
2023-05-31 19:56:12 +0300

    Update scenes environment logic
    Added 'getSceneEntrance' function, that
    returns scene entrance function for a passed
    sceneID

--- source/handlers/scenes/scenes.ts
+++ source/handlers/scenes/scenes.ts
@@ -1,5 +1,12 @@
 import { Composer } from "grammy";
-import mainMenu, { sceneId as mainMenuId } from "./main-menu.scene";
+import mainMenu, {
+  sceneId as mainMenuId,
+  enter as enterMainMenu,
+} from "./main-menu.scene";
+import setSex, {
+  sceneId as setSexId,
+  enter as enterSexSetter,
+} from "./setters/sex.setter.scene";
 import type { CustomContext } from "@src/context";
 
 // - - - - - - - //
@@ -7,12 +14,33 @@ import type { CustomContext } from "@src/context";
 export const sceneIds = {
   none: "NONE",
   mainMenu: mainMenuId,
+  setSex: setSexId,
 };
+
+export function getSceneEntrance(sceneId: string) {
+  switch (sceneId) {
+    case "NONE":
+      throw new Error(
+        "Detected 'NONE' scene ID in 'getSceneEntrance(sceneId: string)' "
+      );
+    case sceneIds.mainMenu:
+      return enterMainMenu;
+    case sceneIds.setSex:
+      return enterSexSetter;
+  }
+
+  throw new Error("Unknown scene ID in 'getSceneEntrance(sceneId: string)' ");
+}
 
 // - - - - - - - //
 
+function route(ctx: CustomContext, sceneToCheck: string) {
+  return ctx.state.scene === sceneToCheck;
+}
+
 const scenes = new Composer<CustomContext>();
 
-scenes.use(mainMenu);
+scenes.filter((ctx: CustomContext) => route(ctx, mainMenuId), mainMenu);
+scenes.filter((ctx: CustomContext) => route(ctx, setSexId), setSex);
 
 export default scenes;
