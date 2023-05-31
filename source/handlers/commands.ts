fd2b14a9e9a2ed66db7aa6dd47110279d7c0228c
Winter4 <mywinter4@gmail.com>
2023-05-31 19:56:28 +0300

    Fullfill /start command

--- source/handlers/commands.ts
+++ source/handlers/commands.ts
@@ -1,7 +1,9 @@
 import { Composer } from "grammy";
 import type { CustomContext } from "@src/context";
 
+import { getSceneEntrance, sceneIds } from "./scenes/scenes";
 import { enter as enterMainMenu } from "./scenes/main-menu.scene";
+import { enter as enterSexSetter } from "./scenes/setters/sex.setter.scene";
 
 const commands = new Composer<CustomContext>();
 
@@ -9,10 +11,25 @@ commands.command("start", async (ctx: CustomContext) => {
   if (ctx.state.registered) {
     return enterMainMenu(ctx);
   } else {
-    await ctx.reply(
-      `Приветствую! Я - бот FitHealth, и я помогу Вам контроллировать и анализировать ваше питание!
+    if (ctx.state.scene === sceneIds.none) {
+      await ctx.reply(
+        `Приветствую! Я - бот FitHealth, и я помогу Вам контроллировать и анализировать ваше питание!
       \nПожалуйста, введите свои данные для регистрации ✍️`
-    );
+      );
+
+      // create models
+      await ctx.db.user.create({ data: { tg_id: ctx.from!.id.toString() } });
+      await ctx.db.state.create({
+        data: { user_tg_id: ctx.from!.id.toString() },
+      });
+
+      return enterSexSetter(ctx);
+    } else {
+      await ctx.reply("Пожалуйста, завершите регистрацию!");
+      const sceneEntrance = getSceneEntrance(ctx.state.scene);
+
+      return sceneEntrance(ctx);
+    }
   }
 });
 
