9a787a277bceae9a8cb49c654a3d11f74303851c
Winter4 <mywinter4@gmail.com>
2023-05-31 19:54:10 +0300

    Add "push" method to cache

--- source/cache.ts
+++ source/cache.ts
@@ -1,5 +1,14 @@
 import { BotClients } from "./settings/clients";
 
+export type refreshCacheFunction = (
+  tgId: string
+) => Promise<{ scene: string; registered: boolean } | null>;
+
+export type updateCacheFunction = (
+  tgId: string,
+  state: { scene?: string; registered?: boolean }
+) => Promise<void>;
+
 export class UserCache {
   constructor(
     private redisClient: BotClients["redis"],
@@ -21,13 +30,14 @@ export class UserCache {
       const state = await this.dbClient.state.findFirst({
         where: { user_tg_id: { equals: tgId } },
       });
+
       if (!state) {
         return null;
       }
       if (!state.scene) {
         throw new Error(`Can't find State.scene for user; TG ID = ${tgId}`);
       }
-      if (!state.registered) {
+      if (typeof state.registered !== "boolean") {
         throw new Error(
           `Can't find State.registered for user; TG ID = ${tgId}`
         );
@@ -42,5 +52,20 @@ export class UserCache {
 
     await this.redisClient.expire(key, 45);
     return JSON.parse(cache);
+  }
+
+  public async push(
+    tgId: string,
+    state: { scene?: string; registered?: boolean }
+  ) {
+    // update data in DB
+    await this.dbClient.state.update({
+      where: { user_tg_id: tgId },
+      data: { ...state },
+    });
+
+    const key = this.generateKey(tgId);
+    await this.redisClient.del(key); // clear the cache (its data is no more valuable)
+    await this.pull(tgId); // refresh the cache
   }
 }
