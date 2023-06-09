cc61040ee8918a668c71e0c35dbbebb8e4f1fb8b
Winter4 <mywinter4@gmail.com>
2023-06-09 16:06:13 +0300

    Add GET /report/uuid handler

--- source/web-api/api/report/report.controller.ts
+++ source/web-api/api/report/report.controller.ts
@@ -1,23 +1,48 @@
 import { ApiClients } from "@src/web-api/types";
-import { Request, Response } from "express";
+import { Request, Response, NextFunction } from "express";
 
 class ReportController {
   constructor(private db: ApiClients["database"]) {}
 
-  public async get(req: Request, res: Response) {
+  public async get(req: Request, res: Response, next: NextFunction) {
     const { uuid } = req.params;
-
-    const todayReport = await this.db.report.findUnique({
-      where: {
-        user_uuid_date: {
-          user_uuid: uuid,
-          date: new Date(),
-        },
+    const reportUniqueKey = {
+      user_uuid_date: {
+        user_uuid: uuid,
+        date: new Date(),
       },
+    };
+
+    const user = await this.db.user.findUnique({ where: { uuid } });
+    if (!user) throw new Error(`Can't find User with uuid = ${uuid}`);
+    if (!user!.calories_limit) {
+      throw new Error(`User.calories_limit = ${user!.calories_limit}`);
+    }
+
+    // check if there is a report for today
+    const todayReportCount = await this.db.report.count({
+      where: reportUniqueKey.user_uuid_date,
     });
 
-    if (!todayReport) {
-    }
+    // update the existing report
+    const todayReport = todayReportCount
+      ? await this.db.report.update({
+          where: reportUniqueKey,
+          data: {
+            calories_limit: user.calories_limit,
+          },
+        })
+      : // or create a new one
+        await this.db.report.create({
+          data: {
+            user_uuid: uuid,
+            calories_limit: user.calories_limit,
+            date: new Date(),
+          },
+        });
+
+    // send updated/created report
+    res.json(todayReport);
   }
 }
 
